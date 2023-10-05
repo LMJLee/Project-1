4c21f3c8324e201d13d69397c5077f605cba907f
Luke Lee <82838982+LMJLee@users.noreply.github.com>
2023-10-05 13:36:21 +0100

    first commit

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from markdown2 import Markdown
+from markdown import Markdown
 
 from . import util
 from . import models
@@ -15,16 +15,21 @@ from django import forms
 
 
 def title(request, title):
-    if title not in util.list_entries():
-        error = HttpResponse("Page not found.")
-        return error
+    entries = [entry.lower() for entry in util.list_entries()]
+    if title not in entries:
+        return index(request)
     content = Markdown().convert(util.get_entry(title))
     return render(request, "../templates/encyclopedia/title.html", {"title": title, "content": content})
 
 def search(request):
     if request.method == 'GET':
         query = request.GET.get('q')
-       
+        entries = [entry.lower() for entry in util.list_entries()]
+        if query not in entries:
+            #If the query does not match the name of an encyclopedia entry, the user should instead be taken to a search results page that displays a 
+            # list of all encyclopedia entries that have the query as a substring. For example, if the search query were ytho, then Python should appear in the search results.
+            Entry.objects.get(headline__contains=query)
+            return render(request, "..")
         if query:
             print("text:", query)
             return title(request, query) 
